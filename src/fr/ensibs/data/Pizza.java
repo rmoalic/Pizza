41dd82c366077929ed42f33762de138c838b19f5
MaximeF <fumelmax@gmail.com>
2019-02-13 16:54:21 +0100

    javadoc

--- src/fr/ensibs/data/Pizza.java
+++ src/fr/ensibs/data/Pizza.java
@@ -1,5 +1,12 @@
 package fr.ensibs.data;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Represents a pizza.
+ */
+@XmlRootElement
 public class Pizza {
 	
 	private int id;
@@ -8,6 +15,13 @@ public class Pizza {
     
     private static int nb = 0;
     
+    private Pizza() {}
+    
+    /**
+     * Construct a pizza.
+     * @param name the pizza name
+     * @param price the pizza price
+     */
     public Pizza(String name, int price) {
     	this.id = nb;
     	nb++;
@@ -15,14 +29,29 @@ public class Pizza {
     	this.price = price;
     }
     
+    /**
+     * Get the pizza id.
+     * @return the pizza id
+     */
+    @XmlElement(name="id")
     public int getId() {
     	return this.id;
     }
     
+    /**
+     * Get the pizza name.
+     * @return the pizza name
+     */
+    @XmlElement(name="name")
     public String getName() {
     	return this.name;
     }
     
+    /**
+     * Get the pizza price.
+     * @return the pizza price
+     */
+    @XmlElement(name="price")
     public int getPrice() {
     	return this.price;
     }
--- src/fr/ensibs/services/OrderServiceImpl.java
+++ src/fr/ensibs/services/OrderServiceImpl.java
@@ -9,27 +9,52 @@ import fr.ensibs.data.Commande;
 import fr.ensibs.data.Pizza;
 import fr.ensibs.data.User;
 
+/**
+ * {@inheritDoc}
+ */
 @WebService(endpointInterface="fr.ensibs.services.OrderService", serviceName="OrderService", portName="OrderPort")
 public class OrderServiceImpl implements OrderService {
 	
 	List<Pizza> pizzas = new ArrayList<>();
 	List<Commande> orders = new ArrayList<>();
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean order(User user, Pizza pizza) {
 		if (user == null)
-			throw new NullPointerException("Person is null");
+			throw new NullPointerException("User is null");
 		else if (pizza == null)
 			throw new NullPointerException("Pizza is null");
 		Commande order = new Commande(user, pizza);
 		return this.orders.add(order);
 	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean addPizza(String name, int price) {
+		if (name.isEmpty())
+			throw new NullPointerException("name is empty");
+		else if (price < 0)
+			throw new NullPointerException("price is negative");
+		Pizza pizza = new Pizza(name, price);
+		return this.pizzas.add(pizza);
+	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<Pizza> getPizzasList() {
 		return this.pizzas;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<Commande> getUserOrdersList(User user) {
 		List<Commande> list = new ArrayList<>();
@@ -37,6 +62,17 @@ public class OrderServiceImpl implements OrderService {
 			if(order.getUser() == user)
 				list.add(order);
 		return list;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Pizza findPizzaById(int id) {
+		for(Pizza pizza : this.pizzas)
+			if(pizza.getId() == id)
+				return pizza;
+		throw new NullPointerException("no pizza with this id");
 	}
 
 }
